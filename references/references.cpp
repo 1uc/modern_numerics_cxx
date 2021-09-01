ad372f0e7cce28def6ef1149e466430387232d51
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-09-01 14:39:00 +0200

    Polish `references`.

--- references/references.cpp
+++ references/references.cpp
@@ -4,13 +4,13 @@
 // In C++ there are three good ways of passing arguments to a function. Either
 // by value, by reference or by const reference.
 //
-// by value: the argument is copied an the function receives a copy of the
+// by value: the argument is copied and the function receives the copy of the
 //           argument. The function is permitted to modify the copy. Naturally
 //           these modifications to the argument will be lost when the function
 //           returns.
 void foo_by_val(std::vector<double> x) {
   x[0] = -42.0;
-  std::cout << x[0] << "\n";
+  std::cout << "foo_by_val: x[0] = " << x[0] << "\n";
 }
 
 // by reference: a 'reference' to the argument is passed to the function, i.e.
@@ -22,12 +22,12 @@ void foo_by_val(std::vector<double> x) {
 //           This is almost exclusively used for output arguments.
 void foo_by_ref(std::vector<double> &x) {
   x[0] = -42.0;
-  std::cout << x[0] << "\n";
+  std::cout << "foo_by_ref: x[0] = " << x[0] << "\n";
 }
 
-// by reference: a 'const reference' is passed to the function. This is just
-//           like a reference, but the function is not permitted to modify the
-//           argument.
+// by const reference: a 'const reference' is passed to the function. This is
+//           just like a reference, but the function is not permitted to modify
+//           the argument.
 //
 //           This is used when you don't want or need a full copy, and don't
 //           want to modify the argument. This is almost always the correct way
@@ -35,19 +35,19 @@ void foo_by_ref(std::vector<double> &x) {
 //           e.g. `double`, `float`, etc.
 void foo_by_cref(const std::vector<double> &x) {
   // x[0] = -42.0;  // Not possible.
-  std::cout << x[0] << "\n";
+  std::cout << "foo_by_cref: x[0] = " << x[0] << "\n";
 }
 
-// The motivation for using a (non-) const reference are clear: either you
+// The motivation for using a (non-) const reference is clear: either you
 // want to modify the object and must use a non-const reference, or you don't
 // want to be able to modify the object and accept a const reference instead.
 
 // Then why do we ever need to pass large objects by value? Well, if we need a
-// copy anyway.  For example because it can only be efficiently implemented if
+// copy anyway. For example because it can only be efficiently implemented if
 // it is allowed to modify its argument, but these modification have no meaning
 // outside of the function. One option would be to allocate a new copy inside
 // the function, or, since there is anyway going to be a copy accept the
-// argument by value.  Sofar nothing gained, but what if the place where the
+// argument by value.  So far nothing gained, but what if the place where the
 // function is called knows it will no longer need the argument? Well, then it
 // can move the argument when passing it to the function.
 
@@ -59,7 +59,7 @@ class Foo {
 public:
   // Since ultimately want a copy of the vector and store it in `x_`. We can
   // make the copy right away. Then instead of copy constructing `x` from `x_`
-  // we can use the move construtor to initialize `x_`.
+  // we can use the move constructor to initialize `x_`.
   Foo(std::vector<double> x) : x_(std::move(x)) {
     // When using this pattern always use a different name for the argument
     // and the member, since at this point, `x` refers to the argument to the
@@ -77,28 +77,31 @@ private:
 int main() {
 
   std::vector<double> x{0.0, 1.0, 2.0};
-  std::vector<double> y{0.0, 1.0, 2.0}; // a spare just in case.
 
+  std::cout << "pre: x[0] = " << x[0] << "\n";
   foo_by_val(x);
-  std::cout << x[0] << "\n";
+  std::cout << "post: x[0] = " << x[0] << "\n";
   std::cout << "--------------------\n";
 
+  std::cout << "pre: x[0] = " << x[0] << "\n";
   foo_by_cref(x);
-  std::cout << x[0] << "\n";
+  std::cout << "post: x[0] = " << x[0] << "\n";
   std::cout << "--------------------\n";
 
+  std::cout << "pre: x[0] = " << x[0] << "\n";
   foo_by_ref(x);
-  std::cout << x[0] << "\n";
+  std::cout << "post: x[0] = " << x[0] << "\n";
   std::cout << "--------------------\n";
 
-  // You're clearly stating that you're handing over ownership of `x` to
-  // `foo_by_val`.
-  foo_by_val(std::move(x));
-  // From now on `x` must not be used again. Tricky.
+  // We don't need `x` anymore and `Foo` can take over the ownership of `x`.
+  auto foo = Foo(std::move(x));
 
-  // A more realistic scenario is when creating a new object.
-  auto foo = Foo(std::move(y)); // We don't need `y` anymore and `Foo` can take
-                                // over the ownership of `x`.
+  // You must not use `x` as of here. This is slippery and exactly the type of
+  // code we want to avoid. The issue is that the name `x` remains valid after
+  // it ceased to be useful. Therefore, avoid having many lines of code where
+  // one could accidentally use `x`.
+  //
+  // Use static code analysis, e.g. clang-tidy, to detect use-after-move.
 
   return 0;
 }
